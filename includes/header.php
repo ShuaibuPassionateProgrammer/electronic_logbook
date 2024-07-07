55ef0244d28c919dc3893aea1d4b40e0ea263392
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-07 06:05:26 +0100

    The header file is ready

---
+++ includes/header.php
@@ -0,0 +1,37 @@
+<section id="social" class="social" style="background: #6a06dd;height: 20px;">
+  <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+          <div class="social-wrapper">
+              <div class="col-md-6">
+                  <div class="social-icon">
+                      <a href="#" style="color: #FFF;"><i class="fa fa-facebook"></i></a>
+                      <a href="#" style="color: #FFF;"><i class="fa fa-twitter"></i></a>
+                      <a href="#" style="color: #FFF;"><i class="fa fa-google-plus"></i></a>
+                      <a href="#" style="color: #FFF;"><i class="fa fa-linkedin"></i></a>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div> <!-- /container -->       
+</section>
+
+
+<nav class="navbar navbar-default" role="navigation">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navHeadercollapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="index.php"> <h3 style="margin-top: -6px;padding-left: 40px;color: #6a06dd;">Electronic Log Book</h3></a>
+ </div>
+
+ <div class="collapse navbar-collapse navHeadercollapse">
+   <ul class="nav navbar-nav navbar-right">
+     <li class="active"><a href="/eleclogbook"><span class="fa fa-home"></span> Home</a></li>
+     <li><a href="login.php"><span class="fa fa-user"></span> Login</a></li>
+     <li><a href="user-account.php"><span class="fa fa-user-plus"></span> Register</a></li>
+   </ul>
+ </div>
+</nav>

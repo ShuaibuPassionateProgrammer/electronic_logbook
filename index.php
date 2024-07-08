cf364834e581739b9af959b1c8fe366f4ffd6a4d
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-08 11:29:46 +0100

    Create a carousel

--- index.php
+++ index.php
@@ -14,5 +14,44 @@
 			<p class="text-center">An electronic log book for student attachment industrial work scheme</p>
 		</div>
 	</header>
+
+    <div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				
+				<div id="carouselIndicators" class="carousel slide mb-3" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+						<li data-target="#carouselIndicators" data-slide-to="1"></li>
+						<li data-target="#carouselIndicators" data-slide-to="2"></li>
+						<li data-target="#carouselIndicators" data-slide-to="3"></li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img src="images/media/1.jpg" class="d-block w-100" style="width:100%; height:425px;">
+						</div>
+						<div class="carousel-item">
+							<img src="images/media/3.jpg" class="d-block w-100" style="width:100%; height:425px;">
+						</div>
+						<div class="carousel-item">
+							<img src="images/media/5.jpg" class="d-block w-100" style="width:100%; height:425px;">
+						</div>
+						<div class="carousel-item">
+							<img src="images/media/6.jpg" class="d-block w-100" style="width:100%; height:425px;">
+						</div>
+					</div>
+					<a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
+				</div>
+
+			</div>
+		</div>
+	</div>
 </body>
 </html>

544f48cac6c3dbddf47124f6c16e38934d29bf1a
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-08 11:27:58 +0100

    Create header with the corresponding content

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
     <link rel="stylesheet" type="text/css" href="css/bootstrap4.min.css">
 </head>
 <body>
-    <!--  -->
+    <header>
+		<div class="container" style="padding: 35px">
+			<img src="images/student_image.png" style="width: 100px; height: 100px;">
+			<h1 class="text-center" style="color: #000">Electronic Logbook</h1>
+			<p class="text-center">An electronic log book for student attachment industrial work scheme</p>
+		</div>
+	</header>
 </body>
 </html>

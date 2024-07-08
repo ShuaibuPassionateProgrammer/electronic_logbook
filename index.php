b6a2488bf1e7a825a5ff75ff5b72ad6932d25554
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-08 11:04:05 +0100

    Link bootstap4 minified css file

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Electronic LogBook</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap4.min.css">
 </head>
 <body>
     <!--  -->

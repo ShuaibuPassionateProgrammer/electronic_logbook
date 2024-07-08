41a4e7c2829d95edf3033c701ac463d8a3a1d86e
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-08 11:32:17 +0100

    Create a custom script

--- index.php
+++ index.php
@@ -63,5 +63,6 @@
     <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
 	<script type="text/javascript" src="js/popper.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap4.min.js"></script>
+    <script></script>
 </body>
 </html>

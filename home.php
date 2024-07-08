12f461134b8ab8c90e635a293316399b45ce84b3
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-08 11:45:09 +0100

    Create a third column third card

--- home.php
+++ home.php
@@ -41,6 +41,19 @@
                     </div>
                 </a>
             </div>
+
+            <div class="col-md-4">
+                <a href="admin-login.php">
+                    <div class="card rounded shadow">
+                        <div class="card-body text-center">
+                            <img src="images/admin_image.png" alt="" class="logo p-2" style="box-shadow: 0 1px 5px rgba(2,2,2,.2); border-radius: 200px">
+                        </div>
+                        <div class="card-body text-center rounded">
+                            <button class="btn btn-outline-primary rounded librarian btn-lg w-50" style="border-radius: 20px">Admin Area</button>
+                        </div>
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
 </body>

609c432524d9f8f34b2a88095e94d4b456776546
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-08 11:51:27 +0100

    Created a topbar

--- student-login.php
+++ student-login.php
@@ -9,6 +9,22 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-    <!--  -->
+    <section id="social" class="social" style="background: #6a06dd;height: 20px;">
+        <div class="container">
+            <!-- Example row of columns -->
+            <div class="row">
+                <div class="social-wrapper">
+                    <div class="col-md-6">
+                        <div class="social-icon">
+                            <a href="#" style="color: #FFF;"><i class="fa fa-facebook"></i></a>
+                            <a href="#" style="color: #FFF;"><i class="fa fa-twitter"></i></a>
+                            <a href="#" style="color: #FFF;"><i class="fa fa-google-plus"></i></a>
+                            <a href="#" style="color: #FFF;"><i class="fa fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- /container -->       
+    </section>
 </body>
 </html>

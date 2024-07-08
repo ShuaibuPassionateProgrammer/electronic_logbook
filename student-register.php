290f31ac410a4c9ad5e51489091ce4110e9af683
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-08 12:20:48 +0100

    Import the bootstrap, icon and custom styles

--- student-register.php
+++ student-register.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo ucfirst(basename($_SERVER['PHP_SELF'], ".php")); ?> | Page</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-    
+    <!--  -->
 </body>
 </html>

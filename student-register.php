3175117372030a468500ca75257925e3e448757c
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-08 12:19:44 +0100

    Create a dynamic title

--- student-register.php
+++ student-register.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Register | Page</title>
+    <title><?php echo ucfirst(basename($_SERVER['PHP_SELF'], ".php")); ?> | Page</title>
 </head>
 <body>
     

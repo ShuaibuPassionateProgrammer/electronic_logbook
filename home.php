585fe86164e40d05c73c2ce04b6ed1301f7f30cf
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-08 11:44:18 +0100

    Create a second column second card

--- home.php
+++ home.php
@@ -28,6 +28,19 @@
                     </div>
                 </a>
             </div>
+
+            <div class="col-md-4">
+                <a href="supervisor-login.php">
+                    <div class="card rounded shadow">
+                        <div class="card-body text-center">
+                            <img src="images/supervisor_image.png" alt="" class="logo p-2" style="box-shadow: 0 1px 5px rgba(2,2,2,.2); border-radius: 200px">
+                        </div>
+                        <div class="card-body text-center rounded">
+                            <button class="btn btn-outline-primary rounded librarian btn-lg w-50" style="border-radius: 20px">Supervisor Area</button>
+                        </div>
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
 </body>

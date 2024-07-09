53b8fb4484212bd881d05029ea252f98a7121867
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-09 22:15:42 +0100

    Bootstrap, css and fontawesome icon imports

--- supervisor-login.php
+++ supervisor-login.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Supervisor Login | Page</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
 </head>
 <body>
-    
+    <!--  -->
 </body>
 </html>

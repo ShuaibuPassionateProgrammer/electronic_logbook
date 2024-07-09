42dc564f6ed66f7a6bf2b4202f34267a43fed383
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-10 00:28:43 +0100

    Fix the bug in the login

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Supervisor Login | Page</title>
+    <title>Student Login | Page</title>
 </head>
 <body>
     

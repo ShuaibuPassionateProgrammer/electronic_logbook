94f7a0975050e736c20c503d0d85e14d48aad95e
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-09 23:11:26 +0100

    Create a top section in a Student REset Password

--- student-reset-password.php
+++ student-reset-password.php
@@ -6,6 +6,22 @@
     <title>Reset Password | Page</title>
 </head>
 <body>
-    
+    <section id="social" class="social" style="background: #6a06dd;height: 20px;">
+        <div class="container">
+            <!-- Example row of columns -->
+            <div class="row">
+                <div class="social-wrapper">
+                    <div class="col-md-6">
+                        <div class="social-icon">
+                            <a href="#" style="color: #FFF;"><i class="fa fa-facebook"></i></a>
+                            <a href="#" style="color: #FFF;"><i class="fa fa-twitter"></i></a>
+                            <a href="#" style="color: #FFF;"><i class="fa fa-google-plus"></i></a>
+                            <a href="#" style="color: #FFF;"><i class="fa fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- /container -->       
+    </section>
 </body>
 </html>

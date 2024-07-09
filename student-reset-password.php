585f826607a07148d4dff92805cdd2a0b0ad4b5f
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-09 23:22:49 +0100

    Create a main panel in a Student REset Password

--- student-reset-password.php
+++ student-reset-password.php
@@ -45,7 +45,9 @@
 
     <div class="container" style="margin-top: 100px;">
         <div class="row">
-            <div class="col-md-4 col-md-offset-4"></div>
+            <div class="col-md-4 col-md-offset-4">
+                <div class="panel panel-default login-panel"></div>
+            </div>
         </div>
     </div>
 </body>

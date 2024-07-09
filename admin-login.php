841d181c2a1ea758909fae1ec3c1777af718e752
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-09 22:45:10 +0100

    Include the error in it also

--- admin-login.php
+++ admin-login.php
@@ -14,6 +14,12 @@
             <div class="row" style="margin-bottom: 65px;">
                 <div class="col-md-12 text-center margin-top: 30px;"><h3 style="text-transform: uppercase; color: #6a06dd;">electronic log book for student attachment industrial work scheme</h3></div>
             </div>
+
+            <div class="col-md-4 col-md-offset-4">
+                <div class="row justify-content-center" style="margin-bottom: 65px; text-align: center">
+                    <?php include('errors.php');?>
+                </div>
+            </div>
         </div>
     </section>
 </body>

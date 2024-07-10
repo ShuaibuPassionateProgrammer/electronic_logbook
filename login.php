0dd7748d2a7ff20a5f1d5c8db21d53fbe3c9a5f8
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-10 02:53:38 +0100

    Crate a main column

--- login.php
+++ login.php
@@ -50,7 +50,9 @@
             </div>
         </div>
 
-        <div class="row"></div>
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4"></div>
+        </div>
     </div>
 </body>
 </html>

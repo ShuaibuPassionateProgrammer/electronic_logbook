60880c75ef4e536eb47cee0d30df843d432be918
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-10 02:56:15 +0100

    Display the panel body in a Student Login

--- login.php
+++ login.php
@@ -56,6 +56,7 @@
                     <div class="panel-heading">
                         <h3 class="panel-title">Sign In</h3>
                     </div>
+                    <div class="panel-body"></div>
                 </div>
             </div>
         </div>

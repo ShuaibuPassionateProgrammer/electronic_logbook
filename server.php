fdd26b822a92dae49c2364479581c990593945b1
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-12 00:59:55 +0100

    Execute the query to register a student

--- server.php
+++ server.php
@@ -102,6 +102,7 @@ if(isset($_POST['newstudent']))
 	    {
             $hash_password = md5($student_password);
             $sql = "INSERT INTO tbl_student(fullname, matricno, level, email, password, status) VALUES ('$student_fullname', '$student_matricno', '$student_level', '$student_email', '$hash_password', 'inactive')";
+            mysqli_query($db, $sql);
         }
     }
 }

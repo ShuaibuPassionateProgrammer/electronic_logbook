afc19386a7ccab66985ee91e4c6fdc8cda56c7dd
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-11 03:55:13 +0100

    Reset student password

--- server.php
+++ server.php
@@ -6,3 +6,13 @@ include('includes/config.php');
 $errors = array();
 $success = array();
 $s_msg = array();
+
+//Reset Student Password
+if(isset($_POST['sturespass']))
+{
+	?>
+	<script>
+		alert("Initializing Student Password...");
+	</script>
+	<?php
+}

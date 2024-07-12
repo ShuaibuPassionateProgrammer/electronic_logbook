b3a6c6f85eccc59039de98e1ac9887f0c1c7c33b
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-12 01:18:44 +0100

    And exit the current script

--- server.php
+++ server.php
@@ -137,6 +137,7 @@ if(isset($_POST['st_login'])) {
             $_SESSION['student'] = $st_matricno;
             $_SESSION['st_login'] = true;
             header('location: student-dashboard.php');
+            exit();
         }
     }
 }

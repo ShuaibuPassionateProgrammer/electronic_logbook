e97000f5ed5d05f02d15c6130fada13e17768577
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-13 11:40:56 +0100

    Done with Reset Password

--- reset-password.php
+++ reset-password.php
@@ -1,24 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Supervisor Reset | Password</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Login | Page</title>
+ 	<link href="css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" />
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
     <?php include('includes/header.php');?>
-    <div class="container" style="margin-top: 100px;">
+	<div class="container" style="margin-top: 100px;">
         <div class="row">
             <div class="col-md-4 col-md-offset-4">
-                <div class="panel panel-default login-panel">
+                <div class="login-panel panel panel-default">
                     <div class="panel-heading">
                         <h3 class="panel-title">Sign In</h3>
                     </div>
                     <div class="panel-body">
-                        <form action="reset-password.php" method="POST">
+                         <form action="reset-password.php" method="POST">
 			                <div class="form-group"><br>
 			                    <input type="text"  class="form-control" placeholder="Email Address" name="email" required>
 			                </div>

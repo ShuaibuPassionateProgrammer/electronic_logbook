58c49ecd10440a205de0898f380316046ae7a647
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-13 11:44:39 +0100

    Done with Student Reset Password

--- student-reset-password.php
+++ student-reset-password.php
@@ -35,12 +35,12 @@
     <span class="icon-bar"></span>
     <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="index.php"> <h3 style="margin-top: -6px;padding-left: 40px;color: #6a06dd;">Electronic Log Book</h3></a>
+    <a class="navbar-brand" href="/eleclogbook"> <h3 style="margin-top: -6px;padding-left: 40px;color: #6a06dd;">Electronic Log Book</h3></a>
 </div>
 
 <div class="collapse navbar-collapse navHeadercollapse">
 <ul class="nav navbar-nav navbar-right">
-    <li><a href="index.php"><span class="fa fa-home"></span> Home</a></li>
+    <li><a href="/eleclogbook"><span class="fa fa-home"></span> Home</a></li>
     <li class="active"><a href="student-login.php"><span class="fa fa-user"></span> Login</a></li>
     <li><a href="student-register.php"><span class="fa fa-user-plus"></span> Register</a></li>
 </ul>

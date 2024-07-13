76a0bd8bb173dcef0053b5196761a9ce11c71325
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-13 11:42:25 +0100

    Done with Student Dashboard

---
+++ student-dashboard.php
@@ -0,0 +1,42 @@
+<?php
+include('server.php');
+include('student_auth.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Home | Page</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<?php include("student-menu.php");?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1"><br>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                        <h5><i class="fa fa-fw fa-check"></i> Brief Historical Background of (SIWES)</h5>
+                    </div>
+                    <div class="panel-body" style="background: #FFF;color: #000;font-size: 15px;border-radius: 0px 0px 5px 5px;">
+                        <h3 style="padding-left: 15px;"><small>Introduction to SIWES</small></h3>
+                        <p style="padding-left:15px;text-align: justify;font-family: Segoe UI;">Students Industrial Work Experience Scheme (SIWES) is one of the industrial training fund (ITF) program. It was designed to give Nigerian students studying occupationally related courses in higher institutions the experience that would supplement their theoretical learning. SIWES unit in the University Maiduguri was established in the year 1986 as an industrial coordinating unit catering for mostly science and some art based students on industrial attachment. The foundation staff were four (4) that comprised an industrial coordinator who is responsible to the Vice Chancellor for the day to day running of the affair of the office, a secretary/typist and an office assistant. SIWES unit was finally and officially commissioned on July, 2006.</p>
+
+                        <br /><h3 style="padding-left: 15px;"><small>Objectives of SIWES</small></h3>
+                        <p style="padding-left: 15px;text-align: justify;font-family: Segoe UI;">The programmes of SIWES are designed to achieve the following objectives:<br>
+                        <span style="color: red;font-size: 15px;">*</span>   Provision of avenue for students to acquire industrial skills and experience during their course of study.<br>
+                        <span style="color: red;font-size: 15px;">*</span>   To prepare students for the work situation they are likely to meet after graduation.<br>
+                        <span style="color: red;font-size: 15px;">*</span>   To expose students to work methods and techniques in handling equipment and machineries that they may not be available in the universities.<br>
+                        <span style="color: red;font-size: 15px;">*</span>   To provide students with an opportunity to apply their theoretical knowledge in real work situations, thereby bridging the gap between theory and practice.</p>
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php');?>

a80a183dc04b8055eb806c930e5474b4fc5973c4
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-13 11:48:05 +0100

    Done with View SIWES notice

---
+++ view-siwes-notice.php
@@ -0,0 +1,55 @@
+<?php 
+include('server.php');
+include('admin_auth.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Manage/View SIWES Notice</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+
+    <div class="row" style="margin-top: 65px;">
+        <div class="col-md-12 text-center"><h3 style="text-transform: uppercase; color: #6a06dd;">Manage/View SIWES Notice</h3></div>
+        <div class="col-md-12 text-center"><h3 style="text-transform: uppercase; color: #6a06dd;">electronic log book for student attachment industrial work scheme</h3></div>
+    </div>
+
+    <?php
+    $sql = "SELECT * FROM tbl_siwes_notice";
+    $query = mysqli_query($db, $sql);
+    $row = mysqli_fetch_assoc($query);
+    ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1"><br>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h5><i class="fa fa-fw fa-check"></i> Manage/View SIWES Acceptance Notice </h5>
+                    </div>
+                    <div class="panel-body" style="background: #FFF;color: #000;font-size: 15px;border-radius: 0px 0px 5px 5px;">
+                        <h3 style="padding-left: 15px;"><small><?php echo $row['title']; ?></small></h3>
+                        <p style="padding-left:15px;text-align: justify;font-family: Segoe UI;">
+                            <?php echo $row['content'];?>
+                        </p>
+                        <p style="padding-left: 15px; text-align: justify; font-family: Segoe UI;">
+                        <button type="button" class="btn btn-primary" id="moreNotice">More Notice</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content">
+            <div class="col-md-10 col-md-offset-1"><br>
+                <a href="admin-dashboard.php">Back to Dashboard</a>
+            </div>
+        </div>
+    </div>
+    <?php
+    ?>
+
+    
+<?php include('includes/footer.php');?>

f09e58a4ad125c2f5dd156bfe14f895a80c96cd1
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-13 11:40:18 +0100

    Done with Menu

---
+++ menu.php
@@ -0,0 +1,66 @@
+<section id="social" class="social" style="background: #4B844B;height: 20px;">
+  <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+          <div class="social-wrapper">
+              <div class="col-md-6">
+                  <div class="social-icon">
+                      <a href="#" style="color: #FFF;"><i class="fa fa-facebook"></i></a>
+                      <a href="#" style="color: #FFF;"><i class="fa fa-twitter"></i></a>
+                      <a href="#" style="color: #FFF;"><i class="fa fa-google-plus"></i></a>
+                      <a href="#" style="color: #FFF;"><i class="fa fa-linkedin"></i></a>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div> <!-- /container -->       
+</section>
+
+
+<nav class="navbar navbar-default" role="navigation">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navHeadercollapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="index.php"> <h5 style="margin-top: -26px;padding-left: 40px;color: green;">Electronic Log Book</h5></a>
+ </div>
+ <div class="collapse navbar-collapse navHeadercollapse">
+   <ul class="nav navbar-nav navbar-right">
+     <li class="active"><a href="student-page.php"><span class="fa fa-home"></span> Home</a></li>
+     <li><a href="registeration.php"><span class="fa fa-user-plus"></span> Registration</a></li>
+
+     <li class="dropdown">
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="fa fa-book"></span> Logbook<b class="caret"></b></a>
+      <ul class="dropdown-menu">
+        <li><a href="add-report.php"><span class="fa fa-plus"></span> Add Daily Report</a></li>
+        <li class="divider"></li>
+        <li><a href="reports.php"><span class="fa fa-table"></span> View All Report</a></li>
+      </ul>
+    </li>
+
+    <li class="dropdown">
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="fa fa-others"></span> Others<b class="caret"></b></a>
+      <ul class="dropdown-menu">
+        <li><a href="#"><span class="fa fa-download"></span> SIWES Latter</a></li>
+        <li class="divider"></li>
+        <li><a href="#"><span class="fa fa-upload"></span> Acceptance Form</a></li>
+        <li class="divider"></li>
+        <li><a href="#"><span class="fa fa-table"></span> Upload Accept. Lttr</a></li>
+        <li class="divider"></li>
+        <li><a href="#"><span class="fa fa-table"></span> Notice</a></li>
+      </ul>
+    </li>
+
+     <li class="dropdown">
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="fa fa-user"></span> Account<b class="caret"></b></a>
+      <ul class="dropdown-menu">
+        <li><a href="login.php"><span class="fa fa-sign-out"></span> Logout</a></li>
+        <li class="divider"></li>
+        <li><a href="change-password.php"><span class="fa fa-lock"></span> Change Password</a></li>
+      </ul>
+    </li>
+   </ul>
+ </div>
+</nav>

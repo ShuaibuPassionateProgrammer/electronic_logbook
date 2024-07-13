78a025e51015d00c5739b20df6f12d789a211ce7
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-13 11:38:53 +0100

    Done with Errors

---
+++ errors.php
@@ -0,0 +1,19 @@
+<?php if(count($errors) > 0): ?>
+	<div class="error">
+		<?php foreach ($errors as $error): ?>
+			<p><?php echo $error; ?></p>
+		<?php endforeach ?>
+	</div>
+<?php endif ?>
+<style type="text/css">
+	.error{
+		width: 92%;
+		margin: 0px auto;
+		padding: 0px 10px;
+		border: 1px solid #a94442;
+		color: #a94442;
+		background: #f2dede;
+		border-radius: 5px;
+		text-align: left;
+	}
+</style>

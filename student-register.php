999ab06270a5e3f1bf3aae8e869ce157f4fc7f4a
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-07-13 11:43:23 +0100

    Done with Student Register

--- student-register.php
+++ student-register.php
@@ -1,57 +1,58 @@
+<?php include("server.php");?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo ucfirst(basename($_SERVER['PHP_SELF'], ".php")); ?> | Page</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?= ucfirst(basename($_SERVER['PHP_SELF'], '.php')); ?> | Page</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" />
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
     <!-- header --> <!-- #280793 -->
     <section id="social" class="social" style="background: #6a06dd;height: 20px;">
-        <div class="container">
-            <!-- Example row of columns -->
-            <div class="row">
-                <div class="social-wrapper">
-                    <div class="col-md-6">
-                        <div class="social-icon">
-                            <a href="#" style="color: #FFF;"><i class="fa fa-facebook"></i></a>
-                            <a href="#" style="color: #FFF;"><i class="fa fa-twitter"></i></a>
-                            <a href="#" style="color: #FFF;"><i class="fa fa-google-plus"></i></a>
-                            <a href="#" style="color: #FFF;"><i class="fa fa-linkedin"></i></a>
-                        </div>
+    <div class="container">
+        <!-- Example row of columns -->
+        <div class="row">
+            <div class="social-wrapper">
+                <div class="col-md-6">
+                    <div class="social-icon">
+                        <a href="#" style="color: #FFF;"><i class="fa fa-facebook"></i></a>
+                        <a href="#" style="color: #FFF;"><i class="fa fa-twitter"></i></a>
+                        <a href="#" style="color: #FFF;"><i class="fa fa-google-plus"></i></a>
+                        <a href="#" style="color: #FFF;"><i class="fa fa-linkedin"></i></a>
                     </div>
                 </div>
             </div>
-        </div> <!-- /container -->       
+        </div>
+    </div> <!-- /container -->       
     </section>
 
-    <nav class="navbar navbar-default" role="navigation">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navHeadercollapse">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="index.php"> <h3 style="margin-top: -6px;padding-left: 40px;color: #6a06dd;">Electronic Log Book</h3></a>
-        </div>
 
-        <div class="collapse navbar-collapse navHeadercollapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="index.php"><span class="fa fa-home"></span> Home</a></li>
-                <li><a href="student-login.php"><span class="fa fa-user"></span> Login</a></li>
-                <li class="active"><a href="student-register.php"><span class="fa fa-user-plus"></span> Register</a></li>
-            </ul>
-        </div>
+    <nav class="navbar navbar-default" role="navigation">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navHeadercollapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php"> <h3 style="margin-top: -6px;padding-left: 40px;color: #6a06dd;">Electronic Log Book</h3></a>
+    </div>
+
+    <div class="collapse navbar-collapse navHeadercollapse">
+    <ul class="nav navbar-nav navbar-right">
+        <li><a href="/eleclogbook"><span class="fa fa-home"></span> Home</a></li>
+        <li><a href="student-login.php"><span class="fa fa-user"></span> Login</a></li>
+        <li class="active"><a href="student-register.php"><span class="fa fa-user-plus"></span> Register</a></li>
+    </ul>
+    </div>
     </nav>
     <!-- /header -->
+	<div class="container" style="margin-top: 50px;">
 
-    <div class="container" style="margin-top: 50px;">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default login-panel">
+                <div class="login-panel panel panel-default">
                     <div class="panel-heading">
                         <h3 class="panel-title">Create New Student Account</h3>
                     </div>
@@ -93,9 +94,11 @@
                                 <button class="btn btn-primary pull-right" type="submit" name="newstudent">&nbsp;Register &nbsp;<span class="fa fa-save"></span></button>
                             </div><!-- Booking tab ends -->
                         </form>
+
                     </div>
                 </div>
             </div>
         </div>
+        
     </div>
 <?php include('includes/footer.php');?>

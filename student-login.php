db2f7ab2d4ba4ed33c34679b573baaa8a6b68422
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-08 22:48:41 +0100

    Student login improved v1

--- student-login.php
+++ student-login.php
@@ -12,59 +12,103 @@ if (isset($_SESSION['st_login'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Login | Electronic Log Book</title>
-    
-    <!-- Styles -->
+    <title>Student Login | Electronic Logbook</title>
+
+    <!-- Bootstrap & Font Awesome -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/style.css">
 
+    <!-- Custom Styling -->
     <style>
         body {
-            background-color: #f8f9fa;
+            background: #f4f7fa;
+            font-family: 'Segoe UI', sans-serif;
         }
 
-        .login-panel {
-            border-radius: 10px;
-            box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);
+        .social-bar {
+            background: #6a06dd;
+            height: 35px;
         }
 
-        .panel-heading {
-            background-color: #6a06dd;
+        .social-bar a {
             color: #fff;
-            border-top-left-radius: 10px;
-            border-top-right-radius: 10px;
+            margin-right: 15px;
+            line-height: 35px;
+            font-size: 14px;
+        }
+
+        .navbar-brand h3 {
+            margin: 0;
+            font-weight: 600;
+            color: #6a06dd;
+        }
+
+        .login-wrapper {
+            max-width: 480px;
+            margin: 60px auto;
+            padding: 40px 30px;
+            background-color: #fff;
+            border-radius: 12px;
+            box-shadow: 0 6px 25px rgba(0, 0, 0, 0.08);
+        }
+
+        .login-wrapper h4 {
+            color: #333;
+            font-weight: 600;
+            margin-bottom: 25px;
+        }
+
+        .form-control {
+            border-radius: 30px;
+        }
+
+        .input-group-text {
+            border-radius: 30px 0 0 30px;
         }
 
         .btn-primary {
             background-color: #6a06dd;
             border: none;
+            border-radius: 30px;
+            padding: 10px 25px;
+            transition: 0.3s ease-in-out;
         }
 
         .btn-primary:hover {
             background-color: #5905bf;
         }
 
-        .input-group-addon {
-            background-color: #eee;
+        .form-footer {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
         }
 
-        #message li {
-            list-style: none;
-            color: red;
+        .alert {
+            border-radius: 8px;
+        }
+
+        .login-wrapper a {
+            color: #6a06dd;
+        }
+
+        @media (max-width: 576px) {
+            .login-wrapper {
+                margin: 30px 15px;
+                padding: 30px 20px;
+            }
         }
     </style>
 </head>
 <body>
 
     <!-- Top Social Bar -->
-    <section style="background: #6a06dd; height: 30px;">
-        <div class="container text-right">
-            <a href="#" class="text-white me-3"><i class="fa fa-facebook"></i></a>
-            <a href="#" class="text-white me-3"><i class="fa fa-twitter"></i></a>
-            <a href="#" class="text-white me-3"><i class="fa fa-google-plus"></i></a>
-            <a href="#" class="text-white"><i class="fa fa-linkedin"></i></a>
-        </div>
+    <section class="social-bar text-right px-4">
+        <a href="#"><i class="fa fa-facebook"></i></a>
+        <a href="#"><i class="fa fa-twitter"></i></a>
+        <a href="#"><i class="fa fa-google-plus"></i></a>
+        <a href="#"><i class="fa fa-linkedin"></i></a>
     </section>
 
     <!-- Navigation -->
@@ -72,15 +116,15 @@ if (isset($_SESSION['st_login'])) {
         <div class="container">
             <div class="navbar-header">
                 <a class="navbar-brand" href="index.php">
-                    <h3 style="margin-top: -6px; color: #6a06dd;">Electronic Log Book</h3>
+                    <h3>Electronic Logbook</h3>
                 </a>
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbarContent">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbarLinks">
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
             </div>
-            <div class="collapse navbar-collapse" id="navbarContent">
+            <div class="collapse navbar-collapse" id="navbarLinks">
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="/eleclogbook"><i class="fa fa-home"></i> Home</a></li>
                     <li class="active"><a href="student-login.php"><i class="fa fa-user"></i> Login</a></li>
@@ -90,61 +134,56 @@ if (isset($_SESSION['st_login'])) {
         </div>
     </nav>
 
-    <!-- Login Form -->
-    <div class="container my-5">
-        <div class="row justify-content-center">
-            <div class="col-md-6 col-lg-5">
-                
-                <?php include('errors.php'); ?>
+    <!-- Login Panel -->
+    <div class="login-wrapper">
+        <h4 class="text-center">Student Sign In</h4>
 
-                <?php if (isset($_SESSION['success'])): ?>
-                    <div class="alert alert-success alert-dismissible fade show" role="alert">
-                        <?= $_SESSION['success']; ?>
-                        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <?php include('errors.php'); ?>
+
+        <?php if (isset($_SESSION['success'])): ?>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <?= $_SESSION['success']; ?>
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+            </div>
+            <?php unset($_SESSION['success']); ?>
+        <?php endif; ?>
+
+        <form action="student-login.php" method="POST">
+            <div class="form-group mb-4">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="fa fa-user"></i></span>
                     </div>
-                    <?php unset($_SESSION['success']); ?>
-                <?php endif; ?>
-
-                <div class="panel panel-default login-panel">
-                    <div class="panel-heading text-center py-3">
-                        <h4>Student Sign In</h4>
-                    </div>
-                    <div class="panel-body p-4">
-                        <form action="student-login.php" method="POST">
-                            <div class="form-group input-group mb-3">
-                                <span class="input-group-addon"><i class="fa fa-user"></i></span>
-                                <input type="text" name="st_matricno" class="form-control" placeholder="Matric Number" required>
-                            </div>
-                            <div class="form-group input-group mb-3">
-                                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
-                                <input type="password" name="st_password" class="form-control" placeholder="Password" required>
-                            </div>
-
-                            <!-- Error Message Output Placeholder -->
-                            <div id="message"><?php /* echo output_message($error); */ ?></div>
-
-                            <div class="d-flex justify-content-between align-items-center mt-3 mb-4">
-                                <a href="student-reset-password.php">Forgot Password?</a>
-                                <button type="submit" name="st_login" class="btn btn-primary">
-                                    <i class="fa fa-sign-in"></i> Login
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-
-                <div class="text-center mt-3">
-                    Don't have an account? <a href="student-register.php"><strong>Register here</strong></a>
+                    <input type="text" name="st_matricno" class="form-control" placeholder="Matric Number" required>
                 </div>
             </div>
+
+            <div class="form-group mb-4">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="fa fa-lock"></i></span>
+                    </div>
+                    <input type="password" name="st_password" class="form-control" placeholder="Password" required>
+                </div>
+            </div>
+
+            <div class="form-footer mb-4">
+                <a href="student-reset-password.php">Forgot Password?</a>
+                <button type="submit" name="st_login" class="btn btn-primary">
+                    <i class="fa fa-sign-in"></i> Login
+                </button>
+            </div>
+        </form>
+
+        <div class="text-center">
+            Don't have an account? <a href="student-register.php"><strong>Register here</strong></a>
         </div>
     </div>
 
     <?php include('includes/footer.php'); ?>
-    
+
     <!-- Scripts -->
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-
 </body>
 </html>

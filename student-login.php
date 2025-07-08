33850248f9188eb14a424c28f19177fdd7c6ee3f
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-08 23:37:30 +0100

    Student login improved v14

--- student-login.php
+++ student-login.php
@@ -168,9 +168,9 @@ if (isset($_SESSION['st_login'])) {
                     <a href="student-register.php" class="text-link"><strong>Register</strong></a>
                 </span>
             
-                <div class="container">
+                <span class="container">
                     <a href="index.php" class="text-center">Back to Homepage</a>
-                </div>
+                </span>
             </div>
         </div>
     </div>

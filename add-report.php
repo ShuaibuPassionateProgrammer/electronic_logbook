5482063252ba2589db4c3906f5d2ef0eba8e602a
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-09 07:40:38 +0100

    Add Report improved

--- add-report.php
+++ add-report.php
@@ -1,6 +1,6 @@
 <?php 
 include('server.php');
-include('student_auth.php');	
+include('student_auth.php');    
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,79 +8,118 @@ include('student_auth.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Report | Page</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+        body {
+            background-color: #f5f7fa;
+        }
+        .panel {
+            border-radius: 8px;
+            box-shadow: 0 4px 12px rgba(0,0,0,0.1);
+        }
+        .panel-heading {
+            background-color: #6a06dd !important;
+            color: #fff !important;
+            border-top-left-radius: 8px;
+            border-top-right-radius: 8px;
+            padding: 15px;
+        }
+        .panel-body {
+            background-color: #fff;
+            padding: 30px;
+        }
+        .form-control {
+            border-radius: 4px;
+            box-shadow: none;
+        }
+        .btn-primary {
+            background-color: #6a06dd;
+            border-color: #6a06dd;
+            transition: background 0.3s ease;
+        }
+        .btn-primary:hover {
+            background-color: #5a04c5;
+            border-color: #5a04c5;
+        }
+        .contact-info p {
+            font-size: 13px;
+            margin-bottom: 8px;
+        }
+        textarea {
+            resize: none;
+        }
+    </style>
 </head>
 <body>
-    <?php include("student-menu.php");?>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1"><br>
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h5 class="fa fa-plus"> Welcome to your Logbook</h5>
-                    </div>
-                    <div class="panel-body">
-                        <div class="row" style="padding-left: 15px;">
-                             <div class="col-md-3">
-                                <form action="add-report.php" method="POST">
-                                    <?php include("errors.php");?>
-                                    <h4 style="margin-top: 30px;text-align: justify; margin-left:14px;">Important Notice</h4>
-                                            <ul class="contact-info col-md-12">
-                                                <p style="font-size: 12px;"><i class="fa fa-bookmark"></i>&nbsp;Make sure that everyday you should add your daily report within 24hrs otherwise you will be absence for that day.
-                                                </p>
-                                                <p><i class="fa fa-phone"></i> + 2348105359871</p>
-                                                <p><i class="fa fa-envelope"></i><a href="#"> info@mysiwes.com</a></p>
-                                                <p><i class="fa fa-globe"></i><a href="#"> www.mysiwes.com</a></p>
-                                            </ul>
-                                        </div>
-                                        <div class="col-md-8 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0" style="margin-top: 30px;">
-                                            <div class="row">
-                                               <div class="form-group col-md-6 has-feedback">
-                                                    <select class="form-control" type="text" name="day" id=""  required="required" autocomplete="off">
-                                                        <option selected disabled>--Select a Day--</option>
-                                                        <option>SUNDAY</option>
-                                                        <option>MONDAY</option>
-                                                        <option>TUESDAY</option>
-                                                        <option>WEDNESDAY</option>
-                                                        <option>THURSDAY</option>
-                                                        <option>FRIDAY</option>
-                                                        <option>SARTUDAY</option>
-                                                    </select>
-                                                </div>
 
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <input class="form-control" placeholder="Date" type="date" name="date">
-                                                    </div>
-                                                </div>
+<?php include("student-menu.php");?>
 
-                                                <div class="col-md-12">
-                                                    <div class="form-group">
-                                                        <input class="form-control" placeholder="Week Number" type="number" name="weekno">
-                                                    </div>
-                                                </div>
-
-
-
-                                                <div class="col-md-12">
-                                                    <div class="form-group">
-                                                        <textarea name="report" class="form-control" id="" cols="30" rows="7" placeholder="Report" ></textarea>
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <button type="submit" name="addreport" class="btn btn-primary" style="float: right;border: 1px solid #FFF;">Save</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+<div class="container" style="margin-top: 40px;">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="fa fa-plus"> &nbsp; Welcome to Your Logbook</h4>
+                </div>
+                <div class="panel-body">
+                    <?php include("errors.php");?>
+                    <form action="add-report.php" method="POST">
+                        <div class="row">
+                            <!-- Sidebar Notice -->
+                            <div class="col-md-4 col-sm-12">
+                                <h4>Important Notice</h4>
+                                <div class="contact-info">
+                                    <p><i class="fa fa-bookmark"></i> You must submit your daily report within 24 hours. Failing to do so may count as absence.</p>
+                                    <p><i class="fa fa-phone"></i> +234 810 535 9871</p>
+                                    <p><i class="fa fa-envelope"></i> <a href="mailto:info@mysiwes.com">info@mysiwes.com</a></p>
+                                    <p><i class="fa fa-globe"></i> <a href="#">www.mysiwes.com</a></p>
                                 </div>
-                            </form>
+                            </div>
+
+                            <!-- Report Form -->
+                            <div class="col-md-8 col-sm-12">
+                                <div class="row">
+                                    <div class="form-group col-md-6">
+                                        <label for="day">Day</label>
+                                        <select name="day" class="form-control" required>
+                                            <option value="" disabled selected>-- Select a Day --</option>
+                                            <option>SUNDAY</option>
+                                            <option>MONDAY</option>
+                                            <option>TUESDAY</option>
+                                            <option>WEDNESDAY</option>
+                                            <option>THURSDAY</option>
+                                            <option>FRIDAY</option>
+                                            <option>SATURDAY</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="date">Date</label>
+                                        <input class="form-control" type="date" name="date" required>
+                                    </div>
+                                    <div class="form-group col-md-12">
+                                        <label for="weekno">Week Number</label>
+                                        <input class="form-control" type="number" name="weekno" placeholder="Week Number" required>
+                                    </div>
+                                    <div class="form-group col-md-12">
+                                        <label for="report">Report</label>
+                                        <textarea name="report" class="form-control" rows="6" placeholder="Write your daily report here..." required></textarea>
+                                    </div>
+                                    <div class="form-group col-md-12 text-right">
+                                        <button type="submit" name="addreport" class="btn btn-primary">
+                                            <i class="fa fa-save"></i> Save Report
+                                        </button>
+                                    </div>
+                                </div>  
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
+                    </form>
+                </div> <!-- /.panel-body -->
+            </div> <!-- /.panel -->
         </div>
     </div>
-<?php include('includes/footer.php');?>
+</div>
+
+<?php include('includes/footer.php'); ?>
+</body>
+</html>

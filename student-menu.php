9c551996b7ac49868845e84fd55b61267a4a4fc1
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-09 07:25:52 +0100

    Student menu improved v2

--- student-menu.php
+++ student-menu.php
@@ -1,7 +1,7 @@
-<section id="social" class="social" style="background: #6a06dd; padding: 8px 0;">
+<section id="social" class="social">
   <div class="container">
     <div class="row">
-      <div class="col-md-12 text-right">
+      <div class="col text-right">
         <div class="social-icon">
           <a href="#" class="social-link"><i class="fa fa-facebook"></i></a>
           <a href="#" class="social-link"><i class="fa fa-twitter"></i></a>
@@ -13,88 +13,130 @@
   </div>
 </section>
 
-<nav class="navbar navbar-default" role="navigation" style="margin-bottom:0;">
+<nav class="navbar navbar-default navbar-static-top">
   <div class="container">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navHeadercollapse" aria-expanded="false" aria-controls="navHeadercollapse">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navHeadercollapse" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
+        <span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="index.php" style="color: #6a06dd; font-weight: 700; font-size: 1.5rem; padding-left: 40px;">
-        Electronic Log Book
-      </a>
+      <a class="navbar-brand" href="index.php">Electronic Log Book</a>
     </div>
 
     <div class="collapse navbar-collapse navHeadercollapse">
       <ul class="nav navbar-nav navbar-right">
-
-        <li class="<?= (basename($_SERVER['PHP_SELF']) == 'student-dashboard.php') ? 'active' : ''; ?>">
-          <a href="student-dashboard.php"><span class="fa fa-home"></span> Home</a>
+        <li class="<?= (basename($_SERVER['PHP_SELF'])=='student-dashboard.php') ? 'active' : '' ?>">
+          <a href="student-dashboard.php"><i class="fa fa-home"></i> Home</a>
         </li>
 
-        <li class="dropdown <?= (in_array(basename($_SERVER['PHP_SELF']), ['add-report.php', 'logbook.php'])) ? 'active' : ''; ?>">
+        <li class="dropdown <?= (in_array(basename($_SERVER['PHP_SELF']), ['add-report.php','logbook.php'])) ? 'active' : '' ?>">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-            <span class="fa fa-book"></span> Logbook <b class="caret"></b>
+            <i class="fa fa-book"></i> Logbook <span class="caret"></span>
           </a>
           <ul class="dropdown-menu">
-            <li><a href="add-report.php"><span class="fa fa-plus"></span> Add Daily Report</a></li>
+            <li><a href="add-report.php"><i class="fa fa-plus"></i> Add Daily Report</a></li>
             <li role="separator" class="divider"></li>
-            <li><a href="logbook.php"><span class="fa fa-table"></span> View Your Logbook</a></li>
+            <li><a href="logbook.php"><i class="fa fa-table"></i> View Your Logbook</a></li>
           </ul>
         </li>
 
-        <li class="dropdown <?= (in_array(basename($_SERVER['PHP_SELF']), ['siwes-letter.php', 'acceptance-form.php', 'notice.php'])) ? 'active' : ''; ?>">
+        <li class="dropdown <?= (in_array(basename($_SERVER['PHP_SELF']), ['siwes-letter.php','acceptance-form.php','notice.php'])) ? 'active' : '' ?>">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-            <span class="fa fa-ellipsis-h"></span> Others <b class="caret"></b>
+            <i class="fa fa-ellipsis-h"></i> Others <span class="caret"></span>
           </a>
           <ul class="dropdown-menu">
-            <li><a href="siwes-letter.php"><span class="fa fa-download"></span> SIWES Letter</a></li>
+            <li><a href="siwes-letter.php"><i class="fa fa-download"></i> SIWES Letter</a></li>
             <li role="separator" class="divider"></li>
-            <li><a href="acceptance-form.php"><span class="fa fa-upload"></span> Acceptance Form</a></li>
+            <li><a href="acceptance-form.php"><i class="fa fa-upload"></i> Acceptance Form</a></li>
             <li role="separator" class="divider"></li>
-            <li><a href="notice.php"><span class="fa fa-table"></span> Notice</a></li>
+            <li><a href="notice.php"><i class="fa fa-table"></i> Notice</a></li>
           </ul>
         </li>
 
-        <li class="dropdown <?= (in_array(basename($_SERVER['PHP_SELF']), ['student-change-password.php', 'student-logout.php'])) ? 'active' : ''; ?>">
+        <li class="dropdown <?= (in_array(basename($_SERVER['PHP_SELF']), ['student-change-password.php','student-logout.php'])) ? 'active' : '' ?>">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-            <span class="fa fa-user"></span> Account <b class="caret"></b>
+            <i class="fa fa-user"></i> Account <span class="caret"></span>
           </a>
           <ul class="dropdown-menu">
-            <li><a href="student-change-password.php"><span class="fa fa-lock"></span> Change Password</a></li>
+            <li><a href="student-change-password.php"><i class="fa fa-lock"></i> Change Password</a></li>
             <li role="separator" class="divider"></li>
-            <li><a href="student-logout.php"><span class="fa fa-sign-out"></span> Logout</a></li>
+            <li><a href="student-logout.php"><i class="fa fa-sign-out"></i> Logout</a></li>
           </ul>
         </li>
-
       </ul>
     </div>
   </div>
 </nav>
 
 <style>
-  /* Social icons styling */
-  .social-icon a.social-link {
+  /* Social bar styling */
+  .social {
+    background: #5a3fc0;
+    padding: 8px 0;
+  }
+  .social-icon .social-link {
     color: #fff;
     margin-left: 12px;
     font-size: 18px;
-    transition: color 0.3s ease;
+    transition: color 0.3s;
   }
-
-  .social-icon a.social-link:first-child {
+  .social-icon .social-link:first-child {
     margin-left: 0;
   }
-
-  .social-icon a.social-link:hover {
-    color: #c2a6ff;
-    text-decoration: none;
+  .social-icon .social-link:hover {
+    color: #d1b3ff;
   }
 
-  /* Navbar brand hover */
-  .navbar-brand:hover {
+  /* Navbar styling */
+  .navbar {
+    border: none;
+    border-radius: 0;
+    margin-bottom: 0;
+    background: #fff;
+    box-shadow: 0 2px 10px rgba(0,0,0,0.05);
+  }
+  .navbar .navbar-brand {
+    color: #5a3fc0;
+    font-weight: 700;
+    font-size: 1.6rem;
+    padding-left: 40px;
+    transition: color 0.3s;
+  }
+  .navbar .navbar-brand:hover {
+    color: #451d99;
     text-decoration: none;
-    color: #4a04b8;
+  }
+  .navbar-nav > li > a {
+    font-size: 1rem;
+    color: #444;
+    padding: 15px 20px;
+    transition: color 0.3s;
+  }
+  .navbar-nav > .active > a {
+    color: #fff;
+    background: #5a3fc0;
+    border-radius: 4px;
+  }
+  .navbar-nav > li > a:hover,
+  .navbar-nav > li > a:focus {
+    color: #5a3fc0;
+    background: transparent;
+  }
+  .dropdown-menu {
+    border-radius: 4px;
+    box-shadow: 0 4px 15px rgba(0,0,0,0.1);
+  }
+  .dropdown-menu > li > a {
+    color: #444;
+    font-size: 0.95rem;
+    padding: 10px 20px;
+  }
+  .dropdown-menu > li > a:hover {
+    background: #f1eefd;
+    color: #5a3fc0;
+  }
+  .divider {
+    margin: 6px 0;
+    background-color: #e5e5e5;
   }
 </style>

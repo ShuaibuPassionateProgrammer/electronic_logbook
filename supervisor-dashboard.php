dbce7aae9d6fbb4d253bf0c43c1f560589ef2fc5
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-09 10:10:45 +0100

    Supervisor improved

--- supervisor-dashboard.php
+++ supervisor-dashboard.php
@@ -1,122 +1,103 @@
 <?php 
 include('server.php');
 include('supervisor_auth.php');
-?>
 
+function showAlert($sessionKey, $alertClass = 'alert-info') {
+    if (isset($_SESSION[$sessionKey])) {
+        $message = htmlspecialchars($_SESSION[$sessionKey]);
+        unset($_SESSION[$sessionKey]);
+        echo <<<HTML
+        <div class="container mt-3">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="alert {$alertClass} alert-dismissible fade show" role="alert">
+                        {$message}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        HTML;
+    }
+}
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<title>Supervisor Dashboard</title>
- 	<link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <style>
+        .dashboard-panel {
+            background-color: #6a06dd;
+            color: white;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+            border-radius: 5px;
+            margin-bottom: 15px;
+            padding: 20px 0;
+            text-align: center;
+            font-weight: 600;
+            text-transform: uppercase;
+            user-select: none;
+        }
+        .dashboard-panel:hover {
+            background-color: #5705b9;
+            text-decoration: none;
+            color: white;
+        }
+    </style>
 </head>
 <body>
 
-    <div class="row" style="margin-top: 65px;">
-        <div class="col-md-12 text-center"><h3 style="text-transform: uppercase; color: #6a06dd;">Welcome to Supervisor Dashboard</h3></div>
-        <div class="col-md-12 text-center"><h3 style="text-transform: uppercase; color: #6a06dd;">electronic log book for student attachment industrial work scheme</h3></div>
-    </div>
+<div class="container mt-5">
 
-    <?php
-    if(isset($_SESSION['uploaded_pics'])) {
-        ?>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 col-md-offset-1">
-                    <div class="alert alert-info alert-dismissible">
-                        <button type="button" class="close" data-dismiss="alert">&times;</button>
-                        <span class="text-center"><?php echo $_SESSION['uploaded_pics']?></span>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <?php
-        unset($_SESSION['uploaded_pics']);
-    }
-    ?>
-
-    <?php
-    if(isset($_SESSION['uploaded_acceptance_letter'])) {
-        ?>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 col-md-offset-1">
-                    <div class="alert alert-success alert-dismissible">
-                        <button type="button" class="close" data-dismiss="alert">&times;</button>
-                        <span class="text-center"><?php echo $_SESSION['uploaded_acceptance_letter']?></span>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <?php
-        unset($_SESSION['uploaded_acceptance_letter']);
-    }
-    ?>
-
-    <?php
-    if(isset($_SESSION['uploaded_notice'])) {
-        ?>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 col-md-offset-1">
-                    <div class="alert alert-success alert-dismissible">
-                        <button type="button" class="close" data-dismiss="alert">&times;</button>
-                        <p class="text-center"><?php echo $_SESSION['uploaded_notice']?></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <?php
-        unset($_SESSION['uploaded_notice']);
-    }
-    ?>
-
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1"><br>
-
-                <div class="panel panel-default" style="background-color: #6a06dd;">
-                    <a href="view-student-activities.php" style="color: white">
-                        <div class="panel-heading text-center">
-                            View Student Activities
-                        </div>
-                    </a>
-                </div>
-
-                <div class="panel panel-default" style="background-color: #6a06dd;">
-                    <a href="upload-siwes-notice.php" style="color: white">
-                        <div class="panel-heading text-center">
-                            Upload SIWES Notice
-                        </div>
-                    </a>
-                </div>
-
-                <div class="panel panel-default" style="background-color: #6a06dd;">
-                    <a href="upload-siwes-acceptance-form.php" style="color: white;">
-                        <div class="panel-heading text-center">
-                            Upload SIWES Acceptance Form
-                        </div>
-                    </a>
-                </div>
-
-                <div class="panel panel-default" style="background-color: #6a06dd;">
-                    <a href="upload-siwes-acceptance-letter.php" style="color: white;">
-                        <div class="panel-heading text-center">
-                            Upload SIWES Acceptance Letter
-                        </div>
-                    </a>
-                </div>
-            
-                <div class="panel panel-default" style="background-color: #6a06dd;">
-                    <a href="supervisor-logout.php" style="color: white;">
-                        <div class="panel-heading text-center">
-                            Logout
-                        </div>
-                    </a>
-                </div>
-            </div>
+    <div class="row mb-4">
+        <div class="col-12 text-center">
+            <h3 class="text-uppercase text-primary" style="color: #6a06dd;">Welcome to Supervisor Dashboard</h3>
+            <h5 class="text-uppercase text-primary" style="color: #6a06dd;">Electronic Log Book for Student Attachment Industrial Work Scheme</h5>
         </div>
     </div>
-<?php include('includes/footer.php');?>
+
+    <?php 
+        showAlert('uploaded_pics', 'alert-info');
+        showAlert('uploaded_acceptance_letter', 'alert-success');
+        showAlert('uploaded_notice', 'alert-success');
+    ?>
+
+    <div class="row justify-content-center">
+        <div class="col-md-6 col-lg-5">
+
+            <a href="view-student-activities.php" class="dashboard-panel d-block mb-3">
+                <i class="fa fa-list-alt"></i> View Student Activities
+            </a>
+
+            <a href="upload-siwes-notice.php" class="dashboard-panel d-block mb-3">
+                <i class="fa fa-upload"></i> Upload SIWES Notice
+            </a>
+
+            <a href="upload-siwes-acceptance-form.php" class="dashboard-panel d-block mb-3">
+                <i class="fa fa-upload"></i> Upload SIWES Acceptance Form
+            </a>
+
+            <a href="upload-siwes-acceptance-letter.php" class="dashboard-panel d-block mb-3">
+                <i class="fa fa-upload"></i> Upload SIWES Acceptance Letter
+            </a>
+
+            <a href="supervisor-logout.php" class="dashboard-panel d-block mb-3">
+                <i class="fa fa-sign-out"></i> Logout
+            </a>
+
+        </div>
+    </div>
+
+</div>
+
+<script src="js/bootstrap.bundle.min.js"></script>
+<?php include('includes/footer.php'); ?>
+
+</body>
+</html>
